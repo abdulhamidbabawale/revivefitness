5457b19c0c3c0ae69384a41937d874e552bd2670
abdulhamidbabawale <hamidbabawale@gmail.com>
2024-10-15 17:49:35 +0100

    add aboutus and membership page url patterns

--- app/brows_site/urls.py
+++ app/brows_site/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns =[
     path("home/",views.home,name='home_view'),
     path("classes/",views.classes,name='classes_view'),
+    path("aboutus/",views.aboutus,name='aboutus_view'),
+    path("membership/",views.membership,name='membership_view'),
 ]
