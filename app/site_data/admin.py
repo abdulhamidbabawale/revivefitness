dcf167cdfc87d7b93b3d9205f9b941331b39c1a3
abdulhamidbabawale <hamidbabawale@gmail.com>
2024-10-20 21:52:59 +0100

    configured models

--- app/site_data/admin.py
+++ app/site_data/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Classes,Plans
+from .models import User,Classes,Plans,PlanDuration,UserProfile,Benefits
 # Register your models here.
 
+admin.site.register(User)
 admin.site.register(Classes)
 admin.site.register(Plans)
+admin.site.register(PlanDuration)
+admin.site.register(UserProfile)
+admin.site.register(Benefits)
