4e40029b9bdb4d4fda20006f51dd637c6fda68f6
abdulhamidbabawale <hamidbabawale@gmail.com>
2024-10-23 22:55:46 +0100

    added login logic to login function

--- app/login_auth/views.py
+++ app/login_auth/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from app.site_data.models import User
+from django.contrib.auth import login as lt,authenticate
+from django.contrib import messages
 
 # Create your views here.
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        try:
+            user = authenticate(request, email=email, password=password)
+        except:
+             messages.error(request, f"User {email} Not Found....")
+             return redirect("home_view")
+        if user is not None:
+            lt(request, user)
+            messages.success(request, "You have successfully logged in!")
+            return redirect("home_view")
+
+        else:
+            messages.error(request, "Username or Password does not match...")
     return render(request,'login.html')
