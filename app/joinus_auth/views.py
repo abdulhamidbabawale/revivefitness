277aa65bdc5444fd6fb3eb0b8c0572ee0f164d4d
abdulhamidbabawale <hamidbabawale@gmail.com>
2024-10-23 22:54:53 +0100

    added demo registration

--- app/joinus_auth/views.py
+++ app/joinus_auth/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from ..site_data.models import User
 
 # Create your views here.
 def p_details(request):
@@ -7,3 +8,11 @@ def joinus_plan(request):
      return render(request,'joinus_plan.html')
 def setpassword(request):
      return render(request,'setpassword.html')
+def reg(request):
+     if request.method == 'POST':
+          email= request.POST.get('email')
+          password= request.POST.get('password')
+          user = User.objects.create_user(email=email, password=password)
+          user.save()
+
+     return render(request,'register.html')
