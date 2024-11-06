b1b2cf0e254ebd0a7ef1cd67c3f92859a2b704c4
abdulhamidbabawale <hamidbabawale@gmail.com>
2024-11-06 08:23:55 +0100

    added render url to allowed hosts

--- gymapp/settings.py
+++ gymapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG','True')
 
-ALLOWED_HOSTS = ['https://revivefitness.onrender.com']
+ALLOWED_HOSTS = ['revivefitness.onrender.com']
 
 
 # Application definition
