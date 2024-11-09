6b5754d1d17c964ca9911becf9b69f4d577f1eea
abdulhamidbabawale <hamidbabawale@gmail.com>
2024-11-09 17:53:43 +0100

    updated cookies config

--- mystaticfiles/temporary_storage.js
+++ mystaticfiles/temporary_storage.js
@@ -1,8 +1,19 @@
+
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+      const date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
 document.querySelectorAll('input[name="duration"]').forEach(input => {
   input.addEventListener('change', function () {
     if (this.checked) { // Ensure this input is the one selected
-      localStorage.setItem('planDuration', this.value); // Save to localStorage
-      console.log("Saved plan duration:", this.value); // Debugging message
+      setCookie('planDuration', this.value); // Save to localStorage
+      console.log("Saved plan duration:", this.value, 1); // Debugging message
     }
   });
 });
@@ -10,6 +21,29 @@ document.querySelectorAll('input[name="duration"]').forEach(input => {
 // Store the selected class and its value
 document.querySelectorAll('input[name="clas"]').forEach(input => {
     input.addEventListener('change', function () {
-        localStorage.setItem('selectedClass', this.value);  // Save selected class to local storage
+      setCookie('selectedClass', this.value, 1);  // Save selected class to local storage
     });
+});
+
+document.querySelectorAll('input[name="plan"]').forEach(input => {
+  input.addEventListener('change', function () {
+    setCookie('selectedplan', this.value, 1);  // Save selected class to local storage
+  });
+});
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const changePlanButton = document.getElementById('change_plan');
+  const selectPlan = document.querySelector('.select_plan');
+  const icon = changePlanButton.querySelector('.fa-chevron-down');
+
+  changePlanButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    selectPlan.classList.toggle('visible');
+    icon.classList.toggle('rotate-icon');
+  });
 });
