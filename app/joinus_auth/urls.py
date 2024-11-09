427d093137e57d97e8ac531338aaba49c3563009
abdulhamidbabawale <hamidbabawale@gmail.com>
2024-11-09 08:28:54 +0100

    update urls

--- app/joinus_auth/urls.py
+++ app/joinus_auth/urls.py
@@ -5,5 +5,5 @@ urlpatterns =[
     path("personaldetails/",views.p_details,name='personaldetails_view'),
     path("plan/",views.joinus_plan,name='joinus_plan_view'),
     path("setpassword/",views.setpassword,name='setpassword_view'),
-    path("reg/",views.reg,name='reg_view')
+    path("reg/",views.reg,name='reg_view'),
 ]
